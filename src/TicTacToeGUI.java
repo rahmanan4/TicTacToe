df4c3d76a108dea59fd55c78a8561c7455ded51d
rahmanan4 <adil.rahmann@gmail.com>
2020-04-05 00:43:34 -0700

    GUI is now working properly, added restart button

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -1,10 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,32 +12,24 @@ import static java.util.Map.entry;
 
 
 public class TicTacToeGUI {
-    // create a frame aka a window
-    private final JFrame gameFrame;
-    // create a board panel which is a class defined below
-    private final BoardPanel boardPanel;
+    private JFrame gameFrame;
+    private BoardPanel boardPanel;
     private JLabel playerTurn = new JLabel();
     private JLabel gameStatus = new JLabel();
-
     private Board ticTacToeBoard;
 
     public TicTacToeGUI(Board b) {
         this.ticTacToeBoard = b;
 
-        // actually instantiate a JFrame to the JFrame being held in the TicTacToeGUI class
         this.gameFrame = new JFrame(Constants.TITLE);
 
-        // adds a border layout where you can then throw various things at various locations of the board.
         this.gameFrame.setLayout(new BorderLayout());
 
-        // adds the menu bar to the game window
         this.gameFrame.setJMenuBar(createMenuBar());
 
-        // make the window dimensions the size of OUTER_FRAME_DIMENSION
         this.gameFrame.setSize(Constants.OUTER_FRAME_DIMENSION);
 
         this.boardPanel = new BoardPanel();
-        // Throws the board panel in the center of the window
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
 
         playerTurn.setText("Player " + ticTacToeBoard.getCurrMove() + "'s Move");
@@ -49,7 +38,6 @@ public class TicTacToeGUI {
         gameStatus.setText("Is game over: " + ticTacToeBoard.isGameOver());
         this.gameFrame.add(gameStatus, BorderLayout.SOUTH);
 
-        // allows you to see the board
         this.gameFrame.setVisible(true);
 
     }
@@ -60,25 +48,41 @@ public class TicTacToeGUI {
         return menuBar;
     }
 
+    private void resetGameFrame(){
+        this.gameFrame.remove(boardPanel);
+
+        this.ticTacToeBoard = new Board();
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+        playerTurn.setText("Player " + ticTacToeBoard.getCurrMove() + "'s Move");
+        this.gameFrame.add(playerTurn, BorderLayout.NORTH);
+        gameStatus.setText("Is game over: " + ticTacToeBoard.isGameOver());
+        this.gameFrame.add(gameStatus, BorderLayout.SOUTH);
+        this.gameFrame.setVisible(true);
+    }
+
     private JMenu createFileMenu(){
-        // create file tab within menu bar
         final JMenu fileMenu = new JMenu(Constants.JMENU_TAB1_NAME);
-        // create exit option underneath file tab
-        final JMenuItem exit = new JMenuItem(Constants.JMENU_TAB1_JMENU_ITEM1);
-        // create action listener to perform some function aka close the program
+
+        final JMenuItem restart = new JMenuItem(Constants.JMENU_TAB1_JMENU_ITEM1);
+        restart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                resetGameFrame();
+            }
+        });
+
+        final JMenuItem exit = new JMenuItem(Constants.JMENU_TAB1_JMENU_ITEM2);
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        // add the exit JMenuItem object to the JMenu object
+
+        fileMenu.add(restart);
         fileMenu.add(exit);
         return fileMenu;
-    }
-
-    public Board getBoard() {
-        return ticTacToeBoard;
     }
 
     private class BoardPanel extends JPanel{
@@ -103,7 +107,6 @@ public class TicTacToeGUI {
     }
 
     private class TilePanel extends JPanel{
-        // when BoardPanel constructor is called, it feeds i from 0 to 8 as the tile id
         private final int tileId;
         private final Map<Integer, String> mapping = Map.ofEntries(
                 entry(0, "00"),
@@ -127,62 +130,49 @@ public class TicTacToeGUI {
             iconX = createTileIcon(Constants.PLAYER_ONE_SIGN);
             iconO = createTileIcon(Constants.PLAYER_TWO_SIGN);
 
-            addMouseListener(new MouseListener() {
-                @Override
+            addMouseListener(new MouseAdapter() {
                 public void mouseClicked(MouseEvent e) {
-                    char currMove;
-                    boolean isValid;
-                    if (ticTacToeBoard.getCurrMove() == 1){
-                        currMove = Constants.PLAYER_ONE_SIGN;
-                        isValid = ticTacToeBoard.move(currMove, mapping.get(tileId));
-                        if (isValid){
-                            assignTileIcon(iconX);
+                    if (!ticTacToeBoard.isGameOver()) {
+                        char currMove;
+                        boolean isValid;
+                        if (ticTacToeBoard.getCurrMove() == 1) {
+                            currMove = Constants.PLAYER_ONE_SIGN;
+                            isValid = ticTacToeBoard.move(currMove, mapping.get(tileId));
+                            if (isValid) {
+                                assignTileIcon(iconX);
+                            }
+                        } else {
+                            currMove = Constants.PLAYER_TWO_SIGN;
+                            isValid = ticTacToeBoard.move(currMove, mapping.get(tileId));
+                            if (isValid) {
+                                assignTileIcon(iconO);
+                            }
                         }
-                    }
-                    else {
-                        currMove = Constants.PLAYER_TWO_SIGN;
-                        isValid = ticTacToeBoard.move(currMove, mapping.get(tileId));
-                        if (isValid){
-                            assignTileIcon(iconO);
+
+                        ticTacToeBoard.gameOver(currMove);
+                        if (ticTacToeBoard.isGameOver()) {
+                            gameStatus.setText(ticTacToeBoard.getWinState() + " Wins!");
+                            //gameStatus.setText("Is game over? " + ticTacToeBoard.isGameOver());
+                            playerTurn.setText("No moves left.");
+
+                        } else {
+                            playerTurn.setText("Player " + ticTacToeBoard.getCurrMove() + "'s Move");
                         }
+                        validate();
                     }
-
-                    ticTacToeBoard.gameOver(currMove);
-                    if (ticTacToeBoard.isGameOver()){
-                        gameStatus.setText("Is game over? " + ticTacToeBoard.isGameOver());
-                        playerTurn.setText("No moves left.");
-
-                    }
-                    else {
-                        playerTurn.setText("Player " + ticTacToeBoard.getCurrMove() + "'s Move");
-                    }
-                    //ticTacToeBoard.printBoard();
-                    validate();
                 }
-
-                @Override
-                public void mousePressed(MouseEvent e) { }
-
-                @Override
-                public void mouseReleased(MouseEvent e) { }
-
-                @Override
-                public void mouseEntered(MouseEvent e) { }
-
-                @Override
-                public void mouseExited(MouseEvent e) { }
             });
             validate();
         }
 
         private void assignTileColor(){
             if(tileId % 2 == 0){
-                //setBackground(Color.lightGray);
-                setBackground(Color.white);
+                setBackground(Color.lightGray);
             }
             else{
                 setBackground(Color.white);
             }
+            setOpaque(true);
         }
 
         private ImageIcon createTileIcon(char c){
@@ -215,6 +205,7 @@ public class TicTacToeGUI {
         private void assignTileIcon(ImageIcon icon){
             this.removeAll();
             JLabel label = new JLabel(icon);
+            label.setOpaque(false);
             add(label);
         }
     }
